5010bdb53501cea1333c1afdc7462ec656b8883c
xubang <3307629938@qq.com>
2022-04-09 19:51:13 +0800

    测试ignre文件

--- src/main/java/com/xubang/LearningApplication.java
+++ src/main/java/com/xubang/LearningApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LearningApplication {
 
     public static void main(String[] args) {
+        System.out.println("hello,world.");
         SpringApplication.run(LearningApplication.class, args);
     }
-
 }
